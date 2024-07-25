79605db32592b38f6662e633b3b3f49602c54d41
FluxHunter <40912698+SilverTree7622@users.noreply.github.com>
2024-07-25 22:26:01 +0900

    [feature] match home line up tab football

---
+++ components/MatchUp/LineUp/Bench.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="div-line-up-bench">
+        <div class="div-2">On the bench</div>
+        <!-- home away info -->
+        <div class="div-3">
+            <div class="div-4-logo-name-left px-auto">
+                <img class="w-[25px] h-[25px]" :src="props.homeLogo" />
+                <span>
+                    {{ props.homeName }}
+                </span>
+            </div>
+            <div class="div-7-logo-name-right px-auto">
+                <img class="w-[25px] h-[25px]" :src="props.awayLogo" />
+                <span>
+                    {{ props.awayName }}
+                </span>
+            </div>
+        </div>
+        <template v-for="(item, idx) in opt.longestList">
+            <div :class="((opt.longestList.length - 1) === idx) ? 'div-17' : 'div-10' ">
+                <!-- home part -->
+                <div class="div-11">
+                    <template v-if="props.homeList[idx]" >
+                        <div class="div-12">
+                            {{ `#${ props.homeList[idx].shirt_number }` }}
+                        </div>
+                        <img
+                            loading="lazy"
+                            :src="props.homeList[idx].logo || '/img/non-available-player1-cirle-1@2x.png'"
+                            class="img"
+                        />
+                        <div class="div-13">
+                            {{ UtilString.convertLong2ShortName(props.homeList[idx].name) }}
+                        </div>
+                    </template>
+                </div>
+                <!-- away part -->
+                <div class="div-14">
+                    <template v-if="props.awayList[idx]" >
+                        <div class="div-12">
+                            {{ `#${ props.awayList[idx].shirt_number }` }}
+                        </div>
+                        <img
+                            loading="lazy"
+                            :src="props.awayList[idx].logo || '/img/non-available-player1-cirle-1@2x.png'"
+                            class="img"
+                        />
+                        <div class="div-16">
+                            {{ UtilString.convertLong2ShortName(props.awayList[idx].name) }}
+                        </div>
+                    </template>
+                </div>
+            </div>
+        </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TMatchUpLineUpPlayer } from '~/types/FootBall/lineUp';
+import UtilString from '~/utils/string';
+
+const props = defineProps<{
+    homeLogo: string;
+    homeName: string;
+    homeList: TMatchUpLineUpPlayer[];
+    awayLogo: string;
+    awayName: string;
+    awayList: TMatchUpLineUpPlayer[];
+}>();
+
+const opt = reactive({
+    longestList: <TMatchUpLineUpPlayer[]> [],
+});
+
+onMounted(async () => {
+    await nextTick();
+    opt.longestList = props.homeList.length > props.awayList.length ? props.homeList : props.awayList;
+});
+</script>
+
+<style scoped>
+.div-line-up-bench {
+  align-self: stretch;
+  border-radius: 6px;
+  background-color: #0c4aa4;
+  display: flex;
+  /* max-width: 360px; */
+  flex-direction: column;
+  color: #000;
+  padding: 0 6px;
+}
+.div-2 {
+  border-radius: 10px 10px 0px 0px;
+  background-color: #0c4aa4;
+  margin-top: 4px;
+  color: #fff;
+  padding: 8px 13px;
+  font: 600 15px Pretendard, sans-serif;
+}
+.div-3 {
+  display: flex;
+  white-space: nowrap;
+  text-align: center;
+}
+.div-4-logo-name-left {
+  align-items: center;
+  border-radius: 6px 0px 0px 0px;
+  background-color: #e0e4ea;
+
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-5 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 500 10px Pretendard, sans-serif;
+}
+.img {
+  aspect-ratio: 1;
+  object-fit: auto;
+  object-position: center;
+  width: 30px;
+  align-self: stretch;
+}
+.div-6 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+
+.div-7-logo-name-right {
+  align-items: center;
+  border-radius: 0px 6px 0px 0px;
+  background-color: #e0e4ea;
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-8 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 500 10px Pretendard, sans-serif;
+}
+.div-9 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+.div-10 {
+  display: flex;
+  white-space: nowrap;
+  text-align: center;
+}
+.div-11 {
+  align-items: center;
+  background-color: #f8f8f8;
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-12 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 800 10px Pretendard, sans-serif;
+  width: 16px;
+  text-align: left;
+  margin-left: 6px;
+}
+.div-13 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+.div-14 {
+  align-items: center;
+  background-color: #f8f8f8;
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-16 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+.div-17 {
+  display: flex;
+  white-space: nowrap;
+  text-align: center;
+}
+.div-18 {
+  align-items: center;
+  border-radius: 0px 0px 0px 6px;
+  background-color: #f8f8f8;
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-19 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 500 10px Pretendard, sans-serif;
+}
+.div-20 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+.div-21 {
+  align-items: center;
+  border-radius: 0px 0px 6px 0px;
+  background-color: #f8f8f8;
+  display: flex;
+  gap: 8px;
+  flex: 1;
+  padding: 4px;
+  border: 1px solid rgba(224, 228, 234, 1);
+}
+.div-22 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 500 10px Pretendard, sans-serif;
+}
+.div-23 {
+  align-self: stretch;
+  margin: auto 0;
+  font: 400 13px Pretendard, sans-serif;
+}
+</style>
--- components/MatchUp/LineUp/index.vue
+++ components/MatchUp/LineUp/index.vue
@@ -22,164 +22,73 @@
                         </div>
                     </div> -->
                 </div>
+                
                 <div class="contents_-football_-4 contents_-football_-6">
-                    <div class="contents_-football_-5 contents_-football_-6">
+                    <div class="contents_-football_-5 contents_-football_-6 h-[820px]">
+                        <img ref="$bg" class="absolute overflow-hidden" :src="props?.bg ?? '/img/Lineup_BG_Football.svg'" @load="onLoadBg"/>
                         <div class="contents_-football contents_">
-                            
-                            <!-- goal keeper -->
-                            <div class="match_-lineup_-player1 match_-lineup_">
-                                <div class="match_-lineup_-player_-upper">
-                                    <div class="overlap-group-2">
-                                        <img class="_-cirle-1" src="/img/erling-haland-cirle@2x.png"
-                                            alt="Erling Haland_Cirle" />
-                                        <div class="player_-grade_9 player_">
-                                            <div class="text-3 valign-text-middle caption">9. 8</div>
-                                        </div>
-                                        <div class="x35-22 valign-text-middle headline3">#29</div>
-                                    </div>
-                                </div>
-                                <div class="b-saka valign-text-middle caption">E.Haland</div>
-                            </div>
-
-                            <!--  -->
-                            <div class="match_-lineup_-player_-line">
-                                <article class="match_-lineup_-player match_-lineup_">
+                            <div
+                                v-for="(playerLine, lineIdx) in fieldOpt.homeList"
+                                class="match_-lineup_-player_-line"
+                            >
+                                <article 
+                                    v-for="(player, line) in playerLine"
+                                    class="match_-lineup_-player match_-lineup_"
+                                >
                                     <div class="match_-lineup_-player_-upper">
                                         <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/darwin-nunez-cirle@2x.png"
-                                                alt="Darwin Nunez_Cirle" />
+                                            <img 
+                                                class="_-cirle-1"
+                                                :src="player.logo ?? '/img/non-available-player1-cirle-1@2x.png'"
+                                                :alt="UtilString.convertLong2ShortName(player.name)"
+                                            />
                                             <div class="player_-grade_9 player_">
-                                                <div class="text-3 valign-text-middle caption">9. 8</div>
+                                                <div class="text-3 valign-text-middle caption">
+                                                    {{ player.rating }}
+                                                </div>
                                             </div>
-                                            <div class="x35-22 valign-text-middle headline3">#7</div>
+                                            <div class="x35-22 valign-text-middle headline3">
+                                                {{ `#${ player.shirt_number }` }}
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="b-saka valign-text-middle caption">D.Nunez</div>
+                                    <div class="b-saka valign-text-middle caption">
+                                        {{ UtilString.convertLong2ShortName(player.name) }}
+                                    </div>
                                 </article>
-                                <article class="match_-lineup_-player match_-lineup_">
+                            </div>
+                        </div>
+                        
+                        <div class="contents_-football contents_">
+                            <div
+                                v-for="(playerLine, lineIdx) in fieldOpt.awayList"
+                                class="match_-lineup_-player_-line-2 match_-lineup_-player_-line-3"
+                            >
+                                <article 
+                                    v-for="(player, line) in playerLine"
+                                    class="match_-lineup_-player match_-lineup_"
+                                >
                                     <div class="match_-lineup_-player_-upper">
                                         <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/leandro-trossard-cirle@2x.png"
-                                                alt="Leandro Trossard_Cirle" />
+                                            <img 
+                                                class="_-cirle-1"
+                                                :src="player.logo ?? '/img/non-available-player1-cirle-1@2x.png'"
+                                                :alt="UtilString.convertLong2ShortName(player.name)"
+                                            />
                                             <div class="player_-grade_9 player_">
-                                                <div class="text-3 valign-text-middle caption">9. 8</div>
+                                                <div class="text-3 valign-text-middle caption">
+                                                    {{ player.rating }}
+                                                </div>
                                             </div>
-                                            <div class="x35-22 valign-text-middle headline3">#9</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">L.Trossard</div>
-                                </article>
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/dominic-solanke-cirle@2x.png"
-                                                alt="Dominic Solanke_Cirle" />
-                                            <div class="player_-grade_8 player_">
-                                                <div class="text-3 valign-text-middle caption">8. 8</div>
+                                            <div class="x35-22 valign-text-middle headline3">
+                                                {{ `#${ player.shirt_number }` }}
                                             </div>
-                                            <div class="x35-22 valign-text-middle headline3">#19</div>
                                         </div>
                                     </div>
-                                    <div class="b-saka valign-text-middle caption">D.Solanke</div>
+                                    <div class="b-saka valign-text-middle caption">
+                                        {{ UtilString.convertLong2ShortName(player.name) }}
+                                    </div>
                                 </article>
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/kai-havertz-cirle@2x.png"
-                                                alt="Kai Havertz_Cirle" />
-                                            <div class="player_-grade_7 player_">
-                                                <div class="text-4 valign-text-middle caption">7. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#8</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">K.Havertz</div>
-                                </article>
-                            </div>
-
-                            <div class="match_-lineup_-player_-line-3">
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/bruno-fernandes-cirle-1@2x.png"
-                                                alt="Bruno Fernandes_Cirle" />
-                                            <div class="player_-grade_6 player_">
-                                                <div class="text-3 valign-text-middle caption">6. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#41</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">B.Fernandes</div>
-                                </article>
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/heung-minson-cirle@2x.png"
-                                                alt="Heung-MinSon_Cirle" />
-                                            <div class="player_-grade_5 player_">
-                                                <div class="text-5 valign-text-middle caption">5. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#4</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">H.Son</div>
-                                </article>
-                            </div>
-
-                            <div class="match_-lineup_-player_-line-3">
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/carlton-morris-cirle-1@2x.png"
-                                                alt="Carlton Morris_Cirle" />
-                                            <div class="player_-grade_4 player_">
-                                                <div class="text-23 valign-text-middle caption">4. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#2</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">C.Morris</div>
-                                </article>
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/non-available-player1-cirle-1@2x.png"
-                                                alt="Non_Available_Player1_Cirle" />
-                                            <div class="player_-grade_3 player_">
-                                                <div class="text-24 valign-text-middle caption">3. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#6</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">B.Saka</div>
-                                </article>
-                                <article class="match_-lineup_-player match_-lineup_">
-                                    <div class="match_-lineup_-player_-upper">
-                                        <div class="overlap-group-2">
-                                            <img class="_-cirle-1" src="/img/matheus-cunha-cirle@2x.png"
-                                                alt="Matheus Cunha_Cirle" />
-                                            <div class="player_-grade_1 player_">
-                                                <div class="text-3 valign-text-middle caption">1. 8</div>
-                                            </div>
-                                            <div class="x35-22 valign-text-middle headline3">#11</div>
-                                        </div>
-                                    </div>
-                                    <div class="b-saka valign-text-middle caption">M.Cunha</div>
-                                </article>
-                            </div>
-
-                            <div class="match_-lineup_-player1 match_-lineup_">
-                                <div class="match_-lineup_-player_-upper">
-                                    <div class="overlap-group-2">
-                                        <img class="_-cirle-1" src="/img/alexander-isak-cirle-1@2x.png"
-                                            alt="Alexander Isak_Cirle" />
-                                        <div class="player_-grade_2 player_">
-                                            <div class="text-3 valign-text-middle caption">2. 8</div>
-                                        </div>
-                                        <div class="x35-22 valign-text-middle headline3">#41</div>
-                                    </div>
-                                </div>
-                                <div class="b-saka valign-text-middle caption">A.Isak</div>
                             </div>
                         </div>
 
@@ -355,14 +264,27 @@
                 </div>
             </div>
         </div>
+        <MatchUpLineUpBench
+            v-if="benchOpt.isExist"
+            class="mx-[10px]"
+            :homeLogo="teamOpt.homeLogo"
+            :homeName="teamOpt.homeName"
+            :homeList="benchOpt.homeList"
+            :awayLogo="teamOpt.awayLogo"
+            :awayName="teamOpt.awayName"
+            :awayList="benchOpt.awayList"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
 import UtilString from '~/utils/string';
 import type { TMatchUpLineUpPlayer } from '~/types/FootBall/lineUp';
-import { TMatchUpStatistics2TitleFootball } from '~/types/FootBall/statistics';
 import type { TMatchUpLineUpSport } from '~/types/lineUp';
+
+const props = defineProps<{
+    bg?: string;
+}>();
 
 const matchUpStore = useMatchUpStore();
 
@@ -384,6 +306,49 @@ const formationOpt = reactive({
     home: <TMatchUpLineUpSport['home_formation']> '',
     away: <TMatchUpLineUpSport['away_formation']> '',
 });
+
+const fieldOpt = reactive({
+    homeList: <TMatchUpLineUpPlayer[][]> [],
+    awayList: <TMatchUpLineUpPlayer[][]> [],
+});
+
+const benchOpt = reactive({
+    isExist: <boolean> false,
+    homeList: <TMatchUpLineUpPlayer[]> [],
+    awayList: <TMatchUpLineUpPlayer[]> [],
+});
+
+const posOpt = reactive({
+    width: <number> 0,
+    height: <number> 0,
+});
+
+const $bg = ref();
+
+const onLoadBg = () => {
+    if (!$bg.value) return;
+    posOpt.width = $bg.value.width;
+    posOpt.height = $bg.value.height;
+};
+
+const setLineUp = (playerList: TMatchUpLineUpPlayer[], isAway: boolean = false) => {
+    const posYTypes = [ ...new Set(playerList.map( player => player.y )) ].sort();
+    const resList: any[] = [];
+    posYTypes.map( () => resList.push([]) );
+    playerList.map((player) => {
+        const idx = posYTypes.findIndex( yType => yType === player.y );
+        resList[idx].push(player);
+    });
+    if (isAway) {
+        resList.forEach( player => player.reverse() );
+        resList.reverse();
+    }
+    return resList;
+};
+
+const setRatingColor = (rating: string): string => {
+    return '';
+};
 
 onMounted(async () => {
     await nextTick();
@@ -412,21 +377,30 @@ onMounted(async () => {
         teamOpt.homeLogo = homeLogo;
         teamOpt.awayLogo = awayLogo;
     }
-    
-    // test
-    console.log('lineup.home: ', lineup.home);
-    console.log(`____________________________________________________________________________`);
-    let cnt = 0;
+    const tmpFieldOptHomeList: TMatchUpLineUpPlayer[] = [];
+    const tmpFieldOptAwayList: TMatchUpLineUpPlayer[] = [];
     lineup.home.map((player) => {
-        if (!player.incidents) return;
-        console.log(`${ UtilString.convertLong2ShortName(player.name) } position: ${ player.x } ${ player.y }`);
-        player.incidents.map((item) => {
-            console.log(`---${ UtilString.convertLong2ShortName(player.name) } / ${ TMatchUpStatistics2TitleFootball[item.type] }`);
-        });
-        cnt++;
+        if (player.x === 0 && player.y === 0) {
+            benchOpt.homeList.push(player);
+        } else {
+            tmpFieldOptHomeList.push(player);
+        }
     });
-    console.log('cnt: ', lineup.home.length - cnt, cnt);
-
+    lineup.away.map((player) => {
+        if (player.x === 0 && player.y === 0) {
+            benchOpt.awayList.push(player);
+        } else {
+            tmpFieldOptAwayList.push(player);
+        }
+    });
+    // position x, y 0 means on the bench
+    if (benchOpt.homeList.length || benchOpt.awayList.length) {
+        benchOpt.isExist = true;
+    } else {
+        benchOpt.isExist = false;
+    }
+    fieldOpt.homeList = setLineUp(tmpFieldOptHomeList);
+    fieldOpt.awayList = setLineUp(tmpFieldOptAwayList, true);
 });
 
 onBeforeUnmount(() => {
